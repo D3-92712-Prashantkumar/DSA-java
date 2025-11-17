1f0a2bf2fc1cf7735e3939de147603df055a5d56
Prashantkumar-Dudhmal <prashantkumardudhmal@gmail.com>
2025-11-17 19:55:04 +0530

    Assignment 1 Q3 CircularList Implementation Without using head DONE!!

--- Assignment1/q3/src/q3/CircularList.java
+++ Assignment1/q3/src/q3/CircularList.java
@@ -36,19 +36,75 @@ public class CircularList {
 			tail = newNode;
 		}
 	}
+	public void addAtPosition(int pos,int data) {
+		Node newNode = new Node(data);
+		Node temp = tail.next;
+		int count = 1;
+		do{
+			if(count+1==pos) {
+				newNode.next = temp.next;
+				temp.next = newNode;
+				return;
+			}
+			System.out.print(" "+temp.data);
+			temp = temp.next;
+			count++;
+		}while(temp!=tail.next);
+		if(temp==tail.next) {
+			System.out.println("Position Out Of Range!!\nAdding Node at the End");
+			addLast(data);
+		}
+	}
 	public void display() {
 		if(tail==null)return;
 		Node temp = tail.next;
-		System.out.println("\ntail: "+tail.data);
-		System.out.print("List : ");
+		System.out.println("\ntail : "+tail.data);
+		System.out.print("List : [");
 		while(temp!=tail){
 			System.out.print(" "+temp.data);
 			temp = temp.next;
 		};
-		System.out.print(" "+tail.data);
+		System.out.print(" "+tail.data+" ]");
 		
 		System.out.println("");
 	}
-	
+	public void deleteFirst() {
+		if(tail==null) {
+			System.out.println("List Already Empty");
+			return;
+		}
+		else {
+			tail.next = tail.next.next;
+		}
+	}
+	public void deleteLast() {
+		if(tail==null) {
+			System.out.println("List Already Empty");
+			return;
+		}
+		else {
+			Node temp = tail;
+			while(temp.next!=tail){
+				temp = temp.next;
+			};
+			temp.next=tail.next;
+			tail = temp;
+		}
+	}
+	public void deleteAtPosition(int pos) {
+		Node temp = tail.next;
+		int count = 1;
+		do{
+			if(count+1==pos) {
+				temp.next = temp.next.next;
+				return;
+			}
+			temp = temp.next;
+			count++;
+		}while(temp!=tail.next);
+		if(temp==tail.next) {
+			System.out.println("Position Out Of Range!!\nNo Node Deleted!");
+		}
+	}
 	
 }
--- Assignment1/q3/src/q3/Program.java
+++ Assignment1/q3/src/q3/Program.java
@@ -1,27 +1,66 @@
 package q3;
 
-public class Program {
+import java.util.Scanner;
 
+public class Program {
+	static Scanner sc = new Scanner(System.in);
+	public static int menu() {
+		System.out.print("-------------------------------"
+				+ "\n0) Exit"
+				+ "\n1) Add at First Position"
+				+ "\n2) Add at Last Position"
+				+ "\n3) Add at a Given Position"
+				+ "\n4) Delete From First Position"
+				+ "\n5) Delete From Last Position"
+				+ "\n6) Delete From Given Position"
+				+ "\n7) Display List"
+				+ "\n   Enter Your Choice: ");
+		return sc.nextInt();
+	}
 	public static void main(String[] args) {
 		CircularList c = new CircularList();
 		
-		c.addLast(5);
-		c.display();
-		c.addFirst(4);
-		c.display();
-		c.addFirst(3);
-		c.display();
-		c.addFirst(2);
-		c.display();
-		c.addFirst(1);
-		c.display();
-		c.addFirst(0);
-		
-		c.display();
-		c.addLast(6);
-		c.display();
-		c.addFirst(0);
-		c.display();
+		int choice = 0;
+		while((choice=menu())!=0) {
+			switch (choice) {
+			case 1:
+				System.out.print("Enter Data : ");
+				c.addFirst(sc.nextInt());
+				c.display();
+				break;
+			case 2:
+				System.out.print("Enter Data : ");
+				c.addLast(sc.nextInt());
+				c.display();
+				break;
+			case 3:
+				System.out.print("Enter Position : ");
+				int pos = sc.nextInt();
+				System.out.print("Enter Data : ");
+				c.addAtPosition(pos, sc.nextInt());
+				c.display();
+				break;
+			case 4:
+				c.deleteFirst();
+				System.out.println("Node Deleted From First Position!!");
+				c.display();
+				break;
+			case 5:
+				c.deleteLast();
+				System.out.println("Node Deleted From Last Position!!");
+				c.display();
+				break;
+			case 6:
+				System.out.print("Enter Data : ");
+				int pos2 = sc.nextInt();
+				c.deleteAtPosition(pos2);
+				System.out.println("Node Deleted From "+pos2+" Position!!");
+				c.display();
+				break;
+			case 7:
+				c.display();
+			}
+		}
 	}
 
 }
